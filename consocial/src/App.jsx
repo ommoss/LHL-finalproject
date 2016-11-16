580574ce2a16d7bd0ff57e1a4657b115929ead55
Owen Moss <ommoss63@gmail.com>
2016-11-16 23:30:08 +0000

    Button Groups added

--- consocial/src/App.jsx
+++ consocial/src/App.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Button from 'react-bootstrap/lib/Button';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 
 class App extends React.Component {
    constructor(props){
@@ -15,12 +16,16 @@ class App extends React.Component {
       <div>
         <div>
           <nav>
-            <h1>Consocial</h1>
+            <h1>Consocial</h1>z
             <ButtonToolbar className = "navRight">
+              <ButtonGroup>
+              <Button>Create tournament</Button>
+              <Button>Find tournament</Button>
+              </ButtonGroup>
+              <ButtonGroup>
               <Button>Sign Up</Button>
               <Button>Login</Button>
-              <Button>Create tournament</Button>
-              <Button className = "btn-primary">Find tournament</Button>
+              </ButtonGroup>
             </ButtonToolbar>
           </nav>
 
