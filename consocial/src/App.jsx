00d41d5b58467cc93ca6772fe0a0b402d5e78c51
Owen Moss <ommoss63@gmail.com>
2016-11-16 22:21:06 +0000

    Server running but nothing inside

---
+++ consocial/src/App.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+   constructor(props){
+      super(props);
+      this.socket = new WebSocket("ws://localhost:4000");
+      this.state = {}
+   }
+
+   render() {
+      return (
+      <div>
+        <div>
+          <nav>
+            <h1>Consocial</h1>
+          </nav>
+        </div>
+        <footer></footer>
+      </div>
+    );
+  }
+}
+
+export default App;
