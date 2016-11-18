8b9a6895a9e8383c3bda51a4850c357f79b24ada
Owen Moss <ommoss63@gmail.com>
2016-11-18 22:05:16 +0000

    geolocation fully functional plus linting

--- consocial/src/main/GoogleMap.jsx
+++ consocial/src/main/GoogleMap.jsx
@@ -2,16 +2,12 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Map, {Marker, InfoWindow} from 'google-maps-react'
 
-
-
-
-
 class GoogleMap extends React.Component {
      constructor(props){
       super(props);
       this.state = {
         google: "",
-        position: {},
+        position: {lat: 41, lng: -180},
         selectedPlace: "LHL"
     }
     this.geoFindMe = this.geoFindMe.bind(this);
@@ -20,41 +16,41 @@ class GoogleMap extends React.Component {
   }
   geoFindMe() {
 
-  if (!navigator.geolocation){
-    console.log("Browser is not compatible");
-    return;
+    if (!navigator.geolocation){
+      console.log("Browser is not compatible");
+      return;
+    }
+    function error() {
+      console.log("error")
+    }
+    navigator.geolocation.getCurrentPosition(this.success, error);
   }
-  function error() {
-  console.log("error")
-   }
-  navigator.geolocation.getCurrentPosition(this.success, error);
-}
   success(position) {
     let latitude  = position.coords.latitude;
     let longitude = position.coords.longitude;
     this.setState({position:
       {lat: latitude,
-        lng: longitude}})
+       lng: longitude}})
     return position = {lat: latitude, lng: longitude}
   }
-    render() {
-      return (
-        <Map
+  render() {
+    return (
+      <Map
         google={window.google}
         onOpen = {this.geoFindMe}
         initialPosition = {this.geoFindMe}
         mapCenter={this.state.position}
         zoom={14}>
-          <Marker
+        <Marker
           name='LHL'
           position={this.state.position} />
-          <InfoWindow
+        <InfoWindow
           onClose={this.onInfoWindowClose}>
             <div>
               <h1>{this.state.selectedPlace.name}</h1>
             </div>
         </InfoWindow>
-        </Map>
+      </Map>
     );
   }
 }
