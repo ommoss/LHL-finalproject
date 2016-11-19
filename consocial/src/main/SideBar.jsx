1be3e614084eba2ae9448bea76c91b1ff40916ca
Todd Ferguson <toddsferg@gmail.com>
2016-11-19 05:08:41 +0000

    add sidebar tournament div info and hardcoded data object in app.js

--- consocial/src/main/SideBar.jsx
+++ consocial/src/main/SideBar.jsx
@@ -4,22 +4,26 @@ import ReactDOM from 'react-dom';
 class SideBar extends React.Component {
     constructor(props){
       super(props);
+
     }
 
+
+
    render() {
+
       return (
-        <div>
+        <div className="sidebar-container">
         <h1>Tournaments near you!</h1>
 
         <div className="well" id="tournament-box">
-            <h3><span id="tournament-name" class ="tourn-info">MarioCart Special</span></h3>
+            <h3><span id="tournament-name" class ="tourn-info">{this.props.data.tournamentName}</span></h3>
             <hr/>
-            <h4>Game Type: <span id="tournament-game-type" class ="tourn-info">MarioCart64</span></h4>
-            <h4>Location: <span id="tournament-location" class ="tourn-info">Garricks Head Pub</span></h4>
-            <h4>Start Time: <span id="tournament-start-time" class ="tourn-info"> 4:00 pm</span></h4>
-            <h4>Date: <span id="tournament-date" class ="tourn-info"> Dec 10, 2016 </span></h4>
-            <h4>Max Players: <span id="tournament-player-max" class ="tourn-info"> 8 </span></h4>
-            <h4>Current Players: <span id="tournament-player-current" class ="tourn-info"> 8 </span></h4>
+            <h4>Game Type: <span id="tournament-game-type" class ="tourn-info">{this.props.data.tournamentLocation}</span></h4>
+            <h4>Location: <span id="tournament-location" class ="tourn-info">{this.props.data.tournamentLocation}</span></h4>
+            <h4>Start Time: <span id="tournament-start-time" class ="tourn-info">{this.props.data.tournamentStart}</span></h4>
+            <h4>Date: <span id="tournament-date" class ="tourn-info">{this.props.data.tournamentDate}</span></h4>
+            <h4>Max Players: <span id="tournament-player-max" class ="tourn-info"> {this.props.data.tournamentPlayerMax} </span></h4>
+            <h4>Current Players: <span id="tournament-player-current" class ="tourn-info"> {this.props.data.tournamentPlayerCurrent} </span></h4>
             <button type="submit" className="btn btn-default">Join Tournament</button>
             <button type="submit" className="btn btn-default">More Details</button>
           </div>
